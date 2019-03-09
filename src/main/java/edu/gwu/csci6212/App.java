e5d977ccec899b3cab962e8b95fc6402158feda3
Sean McBride <spmcbride1201@gmail.com>
2019-03-09 12:05:55 -0500

    Initial commit

---
+++ src/main/java/edu/gwu/csci6212/App.java
@@ -0,0 +1,13 @@
+package edu.gwu.csci6212;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
