67d5494cfe283be25211c31b0557e741c15ccfd5
Sean McBride <spmcbride1201@gmail.com>
2019-03-17 15:47:17 -0400

    chore: Remove zombie import in CoinPickingGame tests

--- src/test/java/edu/gwu/csci6212/CoinPickingGameTest.java
+++ src/test/java/edu/gwu/csci6212/CoinPickingGameTest.java
@@ -4,7 +4,6 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 
 import edu.gwu.csci6212.CoinPickingGame.IllegalCapacity;
